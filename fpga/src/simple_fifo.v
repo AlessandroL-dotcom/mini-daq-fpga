5eb3120386b53f97e6fcd633af4b4c3e86a3dc16
AlessandroL-dotcom <easydo90@gmail.com>
2025-11-07 18:15:03 +0100

    Update simple_fifo.v

--- fpga/src/simple_fifo.v
+++ fpga/src/simple_fifo.v
@@ -1,57 +1,58 @@
-simple_fifo#(
-    parameter DATA_WIDHT = 32,
+module simple_fifo #(
+    parameter DATA_WIDTH = 32,
     parameter DEPTH = 16
 ) (
-    input wire clk, //
+    input wire clk, 
     input wire wr_en,
     input wire rd_en,
     input wire [DATA_WIDTH-1:0] data_in,
     input wire rst,
-    output  reg [DATA_WIDTH-1:0] data_out,
+    output reg [DATA_WIDTH-1:0] data_out,
     output wire full,
     output wire empty 
 );
 
 
-    // ------------------------------
-    // Memoria interna della FIFO
-    // ------------------------------
-    // Array di DEPTH parole, ciascuna larga DATA_WIDTH bit
+    
+    // memoria interna della FIFO
+  
+    // array di DEPTH parole, ciascuna larga DATA_WIDTH bit
    
     reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
 
     
-    // ------------------------------
-    // Puntatori di scrittura e lettura
-    // ------------------------------
+   
+    // puntatori di scrittura e lettura
+    
     // $clog2(DEPTH) calcola quanti bit servono per contare fino a DEPTH-1
     // Esempio: DEPTH=16 → $clog2(16)=4 → wr_ptr è largo 5 bit
+
 
     reg [$clog2(DEPTH):0] wr_ptr = 0;
     reg [$clog2(DEPTH):0] rd_ptr = 0;
 
 
 
-    // ------------------------------
-    // Contatore di elementi attualmente presenti nella FIFO
-    // ------------------------------
+    
+    // contatore di elementi presenti nella FIFO
+   
     // $clog2(DEPTH)+1 garantisce abbastanza bit per contare fino a DEPTH
 
     reg [$clog2(DEPTH)+1:0] count = 0;
 
 
-    // ------------------------------
-    // Stato: FIFO piena o vuota
-    // ------------------------------
+  
+    // stato
+ 
 
 
     assign full = (count == DEPTH);
     assign empty = (count == 0);
 
 
-    // ------------------------------
-    // Logica di scrittura e lettura
-    // ------------------------------
+   
+    // logica 
+   
 
     always @(posedge clk or posedge rst) begin
        if (rst) begin 
@@ -76,6 +77,3 @@ simple_fifo#(
     end
 
 endmodule
-    
- 
-
